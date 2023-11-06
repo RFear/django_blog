cb891c747cb747c0dbc2fc1506e264fb19e44d57
Ryan Fear <rafear91@gmail.com>
2023-11-05 17:27:09 -0800

    Updated polling to class based views as in Lesson 08.

--- polling/views.py
+++ polling/views.py
@@ -1,23 +1,44 @@
 from django.shortcuts import render
 from django.http import Http404
 from polling.models import Poll
+from django.views.generic.list import ListView  # added for class based view
+from django.views.generic.detail import DetailView  # added for class based view
 
-def list_view(request):
-    context = {'polls': Poll.objects.all()}
-    return render(request, 'polling/list.html', context)
+class PollListView(ListView):  # added for class based view
+    model = Poll
+    template_name = 'polling/list.html'
 
-def detail_view(request, poll_id):
-    try:
-        poll = Poll.objects.get(pk=poll_id)
-    except Poll.DoesNotExist:
-        raise Http404
+# def list_view(request):
+#     context = {'polls': Poll.objects.all()}
+#     return render(request, 'polling/list.html', context)
 
-    if request.method == "POST":
+class PollDetailView(DetailView):  # added for class based view
+    model = Poll
+    template_name = 'polling/detail.html'
+
+    def post(self, request, *args, **kwargs):
+        poll = self.get_object()
         if request.POST.get("vote") == "Yes":
             poll.score += 1
         else:
             poll.score -= 1
         poll.save()
 
-    context = {'poll': poll}
-    return render(request, 'polling/detail.html', context)
+        context = {'object': poll}
+        return render(request, 'polling/detail.html', context)
+
+# def detail_view(request, poll_id):
+#     try:
+#         poll = Poll.objects.get(pk=poll_id)
+#     except Poll.DoesNotExist:
+#         raise Http404
+
+#     if request.method == "POST":
+#         if request.POST.get("vote") == "Yes":
+#             poll.score += 1
+#         else:
+#             poll.score -= 1
+#         poll.save()
+
+#     context = {'poll': poll}
+#     return render(request, 'polling/detail.html', context)
